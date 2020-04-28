d64e0f507f0a153ca594de2fb08c6fbd18f2b575
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-04-28 15:48:42 +0800

    Release

--- Editor/Project Thermite/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]){
-    QApplication thermite(argc, argv);
-    MainWindow mainWindow;
-    mainWindow.show();
-    return thermite.exec();
-}
--- Editor/Project Thermite/readme.txt
+++
@@ -1,2 +0,0 @@
-Project Thermite Version 0.7
-By AAAAgito, Lean-Li, MoyuST, PeterLau61, Sylviachelsea, and UndertakerW @CUHKSZ 
