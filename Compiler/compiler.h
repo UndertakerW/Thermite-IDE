765e093086ed04a5e6718e63feadf01e8840aaa7
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2020-05-27 13:33:23 +0800

    Add files via upload

---
+++ Compiler/compiler.h
@@ -0,0 +1,9 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+#include<string>
+#include<iostream>
+
+int compileCode(std::string f_path, std::string e_path);
+int scan_key(std::string firw, std::string line);
+
+#endif // COMPILER_H
