d30ae265340cf6ddf3ff26cb3f807482550a10c4
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-05-30 18:51:49 +0800

    Update Project.exe

---
+++ Editor/Project Thermite/src/brackets.h
@@ -0,0 +1,10 @@
+#ifndef BRACKETS_H
+#define BRACKETS_H
+
+#include <string>
+
+using namespace std;
+
+bool brackets_check(string f_path, string of_path);
+
+#endif // BRACKETS_H
--- Editor/Project Thermite/src/semicolon.cpp
+++ Editor/Project Thermite/src/semicolon.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "semicolon.h"
 
 using namespace std;
 
